1843d0c6ae43b6c983e7dd31be72350cfeffa225
FanenFelix <dnenshater@gmail.com>
2020-08-04 14:55:04 +0300

    created a react quiz

---
+++ react-quiz/src/DisplayTime.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+// function DisplayTime(){
+//     const date = new Date()
+//     const hours = date.getHours()
+//     let timeOfDay
+  
+//     const style = {
+//       color: "#ffffff", 
+//       height: 10,
+//     }
+  
+//     if (hours <12){
+//       timeOfDay = "Morning"
+//       style.color = "yellow"
+  
+//     }
+//     else if (hours >=12 && hours < 17){
+//       timeOfDay ="Afternoon"
+//       style.color = "#ffffff"
+//     }
+//     else {
+//       timeOfDay = "Evening"
+//       style.color = "green"
+//     }
+  
+//     return(
+//       <div>
+//         <h1 style={style}>Good {timeOfDay}!</h1>  
+//       </div>
+//     )
+//   }
+  
+
+
+
+// export default DisplayTime;
